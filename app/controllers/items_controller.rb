a8d1b15edd95004d0d33cdb508ab861f8bd22c8a
mispica <home_3373@yahoo.co.jp>
2023-04-16 16:35:06 +0900

    itemsコントローラーの修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,14 +1,8 @@
 class ItemsController < ApplicationController
-  before_action :move_to_index, except: [:index, :show]
+  before_action :authenticate_user!, except: :index
 
   def index
   end
 
-  private
-  
-  def move_to_index
-    unless user_signed_in?
-      redirect_to action: :index
-    end
-  end
+
 end
