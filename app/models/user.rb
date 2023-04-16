9494c66e682465c41c0b984e40b6698cdf7d5738
mispica <home_3373@yahoo.co.jp>
2023-04-16 16:51:18 +0900

    バリデーションの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,19 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  validates :nickname, :last_name, :first_name, :kana_last_name,:kana_first_name, :birthday, presence: true
-  VALID_PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
-  validates :password, format: { with: VALID_PASSWORD_REGEX }
+  validates :nickname, :birthday, presence: true
+
+  with_options presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角日本語を使用してください' } do
+    validates :first_name
+    validates :last_name
+  end
+
+  with_options presence: true, format: { with: /\A[ァ-ヴー]+\z/, message: '全角カタカナを使用してください' } do
+    validates :kana_first_name
+    validates :kana_last_name
+  end
+
+  
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX, message: 'には英字と数字の両方を含めて設定してください'
 end
