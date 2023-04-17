b90b43ffa69c27a142aca98866967d0dda37986c
mispica <home_3373@yahoo.co.jp>
2023-04-17 21:22:27 +0900

    Active Storageの導入

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -4,5 +4,9 @@ class ItemsController < ApplicationController
   def index
   end
 
+  private
+  
+  def item_params
+    params.reqire(:item).permit(:item_name, :content, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
 
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
+  has_one_attached :image
 
   validates :item_name, :content, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :days_to_ship_id, :price, presence: true
 end
