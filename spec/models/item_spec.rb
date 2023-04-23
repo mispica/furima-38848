071ba3a824f9a28214b3304f7084138683f63c1f
mispica <home_3373@yahoo.co.jp>
2023-04-23 19:06:55 +0900

    販売手数料・販売利益自動計算の実装

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,7 +67,7 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not included in the list")
       end
-      it 'userが紐付いていないと保存できない' do
+      it 'userが紐付いていないと出品できない' do
         @item.user = nil
         @item.valid?
         expect(@item.errors.full_messages).to include('User must exist')
