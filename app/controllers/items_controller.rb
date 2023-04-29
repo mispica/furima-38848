fafa363ad9d8356ab11f66106bdfc59c3d226de6
mispica <home_3373@yahoo.co.jp>
2023-04-29 18:36:22 +0900

    商品情報編集機能修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:index, :show]
+  before_action :set_item, only: [:edit, :show, :update]
 
   def index
     @items = Item.order("created_at DESC")
@@ -18,25 +19,19 @@ class ItemsController < ApplicationController
       end
   end
 
-  def show
-    @item = Item.find(params[:id])
+  def show   
   end
 
   def edit
-    @item = Item.find(params[:id])
     if current_user.id != @item.user_id
       redirect_to root_path
     end
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    if item.save
-      redirect_to item_path(item.id)
+    if @item.update(item_params)
+      redirect_to item_path(@item.id)
     else
-      @item = Item.find(params[:id])
-      @item.update(item_params)
       render :edit
     end
   end
@@ -46,5 +41,9 @@ class ItemsController < ApplicationController
   
   def item_params
     params.require(:item).permit(:item_name, :content, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :days_to_ship_id, :price, :image).merge(user_id: current_user.id)
+  end
+
+  def set_item
+    @item = Item.find(params[:id])
   end
 end
