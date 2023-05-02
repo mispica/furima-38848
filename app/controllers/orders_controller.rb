13ffe1594c28b57ae90e2a5dc28a0f4e321d5be1
mispica <home_3373@yahoo.co.jp>
2023-05-02 21:37:08 +0900

    クレジットカード決済機能実装

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -12,7 +12,9 @@ class OrdersController < ApplicationController
   def create
     @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
+
     if @order_address.valid?
+      pay_item
       @order_address.save
       redirect_to root_path
     else
@@ -23,7 +25,16 @@ class OrdersController < ApplicationController
   private
 
   def order_params
-    params.require(:order_address).permit(:post_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id])
+    params.require(:order_address).permit(:post_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number).merge(user_id: current_user.id, item_id: params[:item_id], token: params[:token])
+  end
+
+  def pay_item
+    Payjp.api_key = ENV["PAYJP_SECRET_KEY"]
+    Payjp::Charge.create(
+    amount: @item.price, 
+    card: order_params[:token], 
+    currency: 'jpy'    
+    )
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :post_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number, :user_id, :item_id
+  attr_accessor :post_code, :prefecture_id, :municipality, :house_number, :building_name, :telephone_number, :user_id, :item_id, :token
 
   with_options presence: true do
     validates :post_code, format: {with: /\A[0-9]{3}-[0-9]{4}\z/, message: "is invalid. Include hyphen(-)"}
@@ -9,6 +9,7 @@ class OrderAddress
     validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Don't include hyphen(-)"}
     validates :user_id
     validates :item_id
+    validates :token, presence: true
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
