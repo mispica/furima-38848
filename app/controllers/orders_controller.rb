09dc39b7e2becfb6e3c1f224b0b0549d8773a7bc
mispica <home_3373@yahoo.co.jp>
2023-05-03 19:55:29 +0900

    商品購入機能実装　修正
    orders_controller.rb

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,8 +1,8 @@
 class OrdersController < ApplicationController
   before_action :authenticate_user!
+  before_action :set_item
 
   def index
-    @item = Item.find(params[:item_id])
     if (current_user.id == @item.user_id) || @item.order
       redirect_to root_path
     end
@@ -10,9 +10,7 @@ class OrdersController < ApplicationController
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_params)
-
     if @order_address.valid?
       pay_item
       @order_address.save
@@ -35,6 +33,12 @@ class OrdersController < ApplicationController
     card: order_params[:token], 
     currency: 'jpy'    
     )
+  end
+
+  private
+
+  def set_item
+    @item = Item.find(params[:item_id])
   end
 
 end
--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -9,7 +9,7 @@ class OrderAddress
     validates :telephone_number, format: {with: /\A[0-9]{10,11}\z/, message: "is invalid. Don't include hyphen(-)"}
     validates :user_id
     validates :item_id
-    validates :token, presence: true
+    validates :token
   end
   validates :prefecture_id, numericality: {other_than: 1, message: "can't be blank"}
 
