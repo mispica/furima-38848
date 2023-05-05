c08f60984c391b8b919cdabcce00b1a6a0a7e36c
mispica <home_3373@yahoo.co.jp>
2023-05-05 18:44:14 +0900

    Update deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -4,6 +4,8 @@ lock "~> 3.17.2"
 set :application, "furima-38848"
 set :repo_url, "git@github.com:mispica/furima-38848.git"
 
+set :branch, "main"
+
 set :linked_dirs, fetch(:linked_dirs, []).push('log', 'tmp/pids', 'tmp/cache', 'tmp/sockets', 'vendor/bundle', 'public/system', 'public/uploads')
 
 set :rbenv_type, :user
